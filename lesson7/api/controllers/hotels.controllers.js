260715236494197cc552479ac73ba1605b090a7f
Nong Suon <nong.suon@gmail.com>
2017-10-27 21:18:16 +0000

    finished lesson 13 MEAN hotel

--- lesson7/api/controllers/hotels.controllers.js
+++ lesson7/api/controllers/hotels.controllers.js
@@ -1,7 +1,17 @@
 var hotelData = require('../data/hotel-data.json')
+
 module.exports.hotelsGetAll = function(req, res) {
   console.log("GET the json");
   res
       .status(200)
       .json( hotelData );
+};
+
+module.exports.hotelsGetOne = function(req, res) {
+  var hotelId = req.params.hotelId;
+  var thisHotel = hotelData[hotelId];
+  console.log("GET hotelId", hotelId);
+  res
+      .status(200)
+      .json( thisHotel );
 };
